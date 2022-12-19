715421dc551bb24b460b21b1c85baa2d2c5ea3ac
Nick Knox <nick@knox.codes>
2022-12-19 12:14:37 -0800

    schema to add score

---
+++ migrations/20221219193727_rent_score/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Places" ADD COLUMN     "gross_rent_score" INTEGER NOT NULL DEFAULT 0;
---
+++ migrations/20221219200038_score_floats/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Places" ALTER COLUMN "gross_rent_score" SET DEFAULT 0,
+ALTER COLUMN "gross_rent_score" SET DATA TYPE DOUBLE PRECISION;
